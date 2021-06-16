cf66d3c10b6fec650913ee79b62abf1782522aff
bazz666 <herrera.se1992@gmail.com>
2021-06-15 20:20:32 -0400

    push to heroku

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,7 +4,6 @@ class Tweet < ApplicationRecord
   has_many  :likes, dependent: :destroy
   validates :content, presence: true
   acts_as_votable
-  has_many :facts
   has_many :retweets ,dependent: :destroy
   #delegate :profile_photo, to: :user, prefix: :true
   
